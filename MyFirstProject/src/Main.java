a7f4bd5f44e6bc5d0707b548d5e8ebbd65e52db3
vinjvalentino <125798951+vinjvalentino@users.noreply.github.com>
2023-03-14 07:19:14 +0000

    Added MyFirstProject

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // sout + tab = shortcut
+        System.out.println("Hello world!");
+        System.out.println("Another line");
+
+        System.out.print("Hello world!");
+        System.out.print(" ...Same line");
+
+        System.out.print("\nHello world!\n");
+        System.out.print("Another line");
+        // This is a comment
+        /*
+           This is a multi line comment
+         */
+    }
+}
