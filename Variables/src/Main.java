61abf64463c9c4c31a1c2a92c098d2f8bc308a05
vinjvalentino <125798951+vinjvalentino@users.noreply.github.com>
2023-03-14 07:28:32 +0000

    Added Variables

---
+++ Variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int x; //Declaration
+        x = 123; //Assignment
+        int y = 123; // Initialisation
+
+        float pi = 3.14f; //Needs to be followed by f
+        double dpi = 3.14;
+
+        char symbol = '@';
+        String name = "Vincent"; //Starts with capital S
+
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(dpi);
+        System.out.println("Hello " + name);
+    }
+}
